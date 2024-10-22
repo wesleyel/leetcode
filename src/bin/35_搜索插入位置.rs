b160ffffa69d103cfe21406bc6831d9cc9037573
magicWenli <yxnian@outlook.com>
2024-10-22 16:08:23 +0800

    Refactor solution for LeetCode problem 35: Optimize search_insert function using binary_search

--- src/bin/35_搜索插入位置.rs
+++ src/bin/35_搜索插入位置.rs
@@ -14,43 +14,43 @@
  * Testcase Example:  '[1,3,5,6]\n5'
  *
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
- * 
+ *
  * 请必须使用时间复杂度为 O(log n) 的算法。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1:
- * 
- * 
+ *
+ *
  * 输入: nums = [1,3,5,6], target = 5
  * 输出: 2
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
- * 
+ *
+ *
  * 输入: nums = [1,3,5,6], target = 2
  * 输出: 1
- * 
- * 
+ *
+ *
  * 示例 3:
- * 
- * 
+ *
+ *
  * 输入: nums = [1,3,5,6], target = 7
  * 输出: 4
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示:
- * 
- * 
+ *
+ *
  * 1 <= nums.length <= 10^4
  * -10^4 <= nums[i] <= 10^4
  * nums 为 无重复元素 的 升序 排列数组
  * -10^4 <= target <= 10^4
- * 
- * 
+ *
+ *
  */
 
 struct Solution;
@@ -59,7 +59,8 @@ fn main() {}
 // @lc code=start
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        nums.iter().take_while(|&&x| x < target).count() as i32
+        // If the value is found then Result::Ok is returned, containing the index of the matching element. If there are multiple matches, then any one of the matches could be returned. The index is chosen deterministically, but is subject to change in future versions of Rust. If the value is not found then Result::Err is returned, containing the index where a matching element could be inserted while maintaining sorted order.
+        nums.binary_search(&target).unwrap_or_else(|x| x) as i32
     }
 }
 // @lc code=end
@@ -67,12 +68,11 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
-	
-	#[test]
+
+    #[test]
     fn test() {
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2);
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 2), 1);
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4);
-	}
+    }
 }
-
