2b6706c6112c53c5a8705ba1de150036d17f5ace
magicWenli <yxnian@outlook.com>
2024-10-22 15:59:40 +0800

    Add solution for LeetCode problem 28: Find the Index of the First Occurrence in a String in Rust

---
+++ src/bin/28_找出字符串中第一个匹配项的下标.rs
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=rust
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ *
+ * https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ *
+ * algorithms
+ * Easy (44.25%)
+ * Likes:    2302
+ * Dislikes: 0
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.6M
+ * Testcase Example:  '"sadbutsad"\n"sad"'
+ *
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0
+ * 开始）。如果 needle 不是 haystack 的一部分，则返回  -1 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：haystack = "sadbutsad", needle = "sad"
+ * 输出：0
+ * 解释："sad" 在下标 0 和 6 处匹配。
+ * 第一个匹配项的下标是 0 ，所以返回 0 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：haystack = "leetcode", needle = "leeto"
+ * 输出：-1
+ * 解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= haystack.length, needle.length <= 10^4
+ * haystack 和 needle 仅由小写英文字符组成
+ * 
+ * 
+ */
+
+
+struct Solution;
+fn main() {}
+
+// @lc code=start
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        haystack.find(&needle).map_or(-1, |i| i as i32)
+    }
+}
+// @lc code=end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+	
+	#[test]
+    fn test() {
+        assert_eq!(Solution::str_str("sadbutsad".to_string(), "sad".to_string()), 0);
+        assert_eq!(Solution::str_str("leetcode".to_string(), "leeto".to_string()), -1);
+	}
+}
+
