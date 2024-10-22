c200311554cd0da890c4f4fe98ce439dfcedceaf
magicWenli <yxnian@outlook.com>
2024-10-22 16:07:33 +0800

    Add solution for LeetCode problem 35: Search Insert Position in Rust

---
+++ src/bin/35_搜索插入位置.rs
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=rust
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode.cn/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (47.13%)
+ * Likes:    2385
+ * Dislikes: 0
+ * Total Accepted:    1.6M
+ * Total Submissions: 3.3M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: nums = [1,3,5,6], target = 7
+ * 输出: 4
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 为 无重复元素 的 升序 排列数组
+ * -10^4 <= target <= 10^4
+ * 
+ * 
+ */
+
+struct Solution;
+fn main() {}
+
+// @lc code=start
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        nums.iter().take_while(|&&x| x < target).count() as i32
+    }
+}
+// @lc code=end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+	
+	#[test]
+    fn test() {
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2);
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 2), 1);
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4);
+	}
+}
+
